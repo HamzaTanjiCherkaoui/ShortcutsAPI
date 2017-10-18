1b39eb8bb0115925d3004d55b00a24f35a16f907
hamza Tanji Cherkaoui <hoxa1235@gmail.com>
2017-10-18 17:10:27 +0100

    adding get shortcuts , and single shortcut to the API

--- app/routes/shortcut.routes.js
+++ app/routes/shortcut.routes.js
@@ -1,4 +1,34 @@
+var ObjectID = require('mongodb').ObjectID;
+
+
 module.exports = function (app,db) {
+	
+	app.get('/shortcuts' , (req,res)=> {
+		
+		db.collection('shortcuts').find({}).toArray(function(error, items) {
+			if (error) {
+				res.send({'error' :`An error has occured while retreving the shortcuts `});
+			} else {
+				res.send(items);	
+			}
+
+			
+		});
+
+	})
+
+	app.get('/shortcuts/:id' , (req,res)=> {
+		const id = req.params.id;
+		const details = {'_id' : new ObjectID(id) };
+		db.collection('shortcuts').findOne(details, (err,item)=> {
+			if(err) {
+				res.send({'error' :`An error has occured while retreving the shortcut ${id} `});
+			} else {
+				res.send(item);
+			}
+		})
+
+	})
 
 	app.post('/shortcuts' , (req,res) => {
 		const shortcut = {
@@ -10,7 +40,7 @@ module.exports = function (app,db) {
 			gif : "/test.gif" };
 			db.collection('shortcuts').insert(shortcut , (err,result)=> {
 				if(err) {
-					res.send({'error' : 'An error has occurred'});
+					res.send({'error' : 'An error has occurred while inserting the shortcut '});
 				} else {
 					res.send(result.ops[0]);
 				}
@@ -18,7 +48,17 @@ module.exports = function (app,db) {
 			})
 		})
 
-	app.get('/shortcuts' , (req,res)=> {
-		res.send('Hello');
-	})
+	
+
+	app.delete('/shortcuts/:id', (req, res) => {
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id) };
+		db.collection('shortcuts').remove(details, (err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(`shortcut ${id} deleted!`);
+			} 
+		});
+	});
 };
