85ddad4f467367d330e378104703bcece34a1cd2
hamza Tanji Cherkaoui <hoxa1235@gmail.com>
2017-10-18 16:57:38 +0100

    adding shortcuts insertion

--- app/routes/shortcut.routes.js
+++ app/routes/shortcut.routes.js
@@ -1,5 +1,24 @@
 module.exports = function (app,db) {
-app.get('/shortcuts' , (req,res)=> {
-	res.send('Hello');
-})
+
+	app.post('/shortcuts' , (req,res) => {
+		const shortcut = {
+			buttons : ["chtr" , "A"] ,
+			description : "description " ,
+			maker : "lorem ipsum" , 
+			rates : 2,
+			views : 12,
+			gif : "/test.gif" };
+			db.collection('shortcuts').insert(shortcut , (err,result)=> {
+				if(err) {
+					res.send({'error' : 'An error has occurred'});
+				} else {
+					res.send(result.ops[0]);
+				}
+
+			})
+		})
+
+	app.get('/shortcuts' , (req,res)=> {
+		res.send('Hello');
+	})
 };
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+	url : 'mongodb://shortcuts-id-db:shortcutsio@ds125195.mlab.com:25195/shortcuts-id-db'
+}
