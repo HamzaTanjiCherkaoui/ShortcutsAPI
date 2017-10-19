982388a72eb9f964926c02b168ba6a18c9e7090a
hamza Tanji Cherkaoui <hoxa1235@gmail.com>
2017-10-19 07:56:48 +0100

    adding jwt-simple and morgan

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const app = express();
 
 const port = 8000;
 
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
